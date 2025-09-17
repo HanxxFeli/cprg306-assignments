0991214e2ecfdb42c4f12f98892df5be5fdef5d0
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-09-17 15:34:15 -0600

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2">Go to the Week 2 page</Link>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function StudentInfo() { 
+    return ( 
+        <div> 
+            <p>Hans Feliciano</p>
+            <span>
+                Link to my Github: <Link href="https://github.com/hanxxfeli">https://github.com/hanxxfeli</Link>
+            </span>
+        </div>
+    )
+}
